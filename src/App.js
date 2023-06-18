f9439dd4a02a45d02e6e8703bc6127b8591e37fc
tejas <tejasgade174@gmail.com>
2023-06-18 20:27:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+  const [message, setMessage] = useState("");
+
+  let calBmi = (e) => {
+    e.preventDefault();
+    if (weight === 0 || height === 0) {
+      alert('Please enter a valid weight and height')
+    } else {
+      let bmi = (weight / (height * height) * 703)
+      setBmi(bmi.toFixed(1))
+ 
+      // Logic for message
+ 
+      if (bmi < 25) {
+        setMessage('You are underweight')
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage('You are a healthy weight')
+      } else {
+        setMessage('You are overweight')
+      }
+    }
+  }
+
+  let reload = () => {
+    window.location.reload();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_container">
+      <h1>BMI Calculator</h1>
+      <form onSubmit={calBmi}>
+        <div className="input_container">
+          <div>
+            <label className="label">Weight (lbs)</label>
+            <input
+           
+              type="text"
+              value={weight}
+              placeholder="Enter Weight..!"
+              className="input"
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </div>
+          <div>
+            <label className="label">Height (in)</label>
+            <input
+              type="text"
+              value={height}
+              placeholder="Enter number..!"
+              className="input"
+              onChange={(e) => setHeight(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="btn_container">
+          <div>
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </div>
+          <div>
+            <button type="reload" className="btn_1" onClick={reload}>
+              Reload
+            </button>
+          </div>
+        </div>
+        <div className="message">
+          <h4>Your BMI is:{bmi}</h4>
+          <p>{message}</p>
+        </div>
+      </form>
     </div>
   );
 }
